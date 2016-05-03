2fd0f39a85626517c4366bd8b2813d6e0b2c63aa
gerauf <fo4786@gmail.com>
2016-05-03 19:15:43 +0100

    save_links

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,15 @@ class Bookmark_manager < Sinatra::Base
     erb :links
   end
 
+  get '/links/new' do
+    erb :link_save
+  end
+
+  post '/links/save' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/links'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
