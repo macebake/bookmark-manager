55703904dad93c9f8bdef997ef2e056849cef065
Daniel Ortiz <danielortiz@Daniels-MacBook-Pro-11.local>
2016-05-05 09:35:55 +0100

    finished stage 15

--- app.rb
+++ app.rb
@@ -18,7 +18,6 @@ class Bookmark < Sinatra::Base
   post '/links' do
     link = Link.create(title: params[:title], url: params[:url])
     tag = Tag.create(name: params[:tags])
-    p tag
     link.tags << tag
     link.save
     redirect '/links'
